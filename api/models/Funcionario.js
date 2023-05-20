34504e447cdac7ea6e15fe34b79053a7e19c2c3d
Vitor Hugo <vitorhugo7b@gmail.com>
2023-05-20 12:16:32 -0300

    Add Banco Sequelize Funcionarios

--- api/models/Funcionario.js
+++ api/models/Funcionario.js
@@ -1,24 +1,103 @@
-export class Funcionario{
-    constructor(id, nome, email, cpf, dataNasc, telefone, usuarioAdmin, senha, pis){
-        this.id = id
-        this.nome = nome
-        this.email = email
-        this.cpf = cpf
-        this.dataNasc = dataNasc
-        this.telefone = telefone
-        this.usuarioAdmin = usuarioAdmin
-        this.senha = senha
-        this.pis = pis
+import { Sequelize } from "sequelize"
+import db from "../db.js"
+
+const Funcionario = db.define('funcionario',{
+
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }, 
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    cpf: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    dataNasc: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    telefone: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    usuarioAdmin: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    senha: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    pis: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
-}
+})
 
-export const getAll = () => {
-    return dbFuncionarios
-}
 
-export const dbFuncionarios = [
-    new Funcionario(1, "Pedro", "pedro@email.com", "12345678911", "123456789", "31-12-1998", "12345678", "rua endereco, nº 23", "Indaiatuba", "SP", "123456", "adminpedro", "senha123", "123456789"),
-    new Funcionario(2, "Vitor", "vitor@email.com", "98765432100", "987654321", "04-02-2003", "12345678", "rua avenida, nº 45", "Indaiatuba", "SP", "654321", "adminvitor", "senha123", "123456789"),
-    new Funcionario(3, "Sabrina", "sabrina@email.com", "45678912310", "456789123", "23-02-2000", "12345678", "rua praça, nº 4", "Indaiatuba", "SP", "456781", "adminsabrina", "senha123", "123456789"),
-    new Funcionario(4, "Amanda", "amanda@email.com", "45675846930", "359829123", "14-05-2001", "12345678", "rua praça, nº 4", "Indaiatuba", "SP", "456781", "adminamanda", "senha123", "123456789")
-] 
+export default Funcionario
+
+// export class Funcionario{
+//     constructor(id, nome, email, cpf, dataNasc, telefone, usuarioAdmin, senha, pis){
+//         this.id = id
+
+//         this.nome = nome
+//         this.email = email
+//         this.cpf = cpf
+//         this.dataNasc = dataNasc
+//         this.telefone = telefone
+//         this.usuarioAdmin = usuarioAdmin
+//         this.senha = senha
+//         this.pis = pis
+//     }
+// }
+
+// let idAtual = 4
+
+// export const create = (funcionario) => {
+//     idAtual++
+//     funcioanario.id = idAtual
+//     dbFuncionarios.push(funcionario)
+//     return funcionario
+// }
+// export const findByPk = (id) =>{
+//     return dbFuncionarios.find(funcioanario => funcioanario.id === id)
+// }
+
+// export const findAll =() => {
+//     return dbFuncionarios
+// }
+
+// export const destroy = (id) =>{
+//     const funcioanario = findByPk(id)
+//     if(!funcioanario){
+//         return false
+//     }
+//     const index = dbFuncionarios.indexOf(funcionario)
+//     dbFuncionarios.splice(index,1)
+//     return true 
+// }
+// export const update = (id,funcionarioUpdate) =>{
+//     const funcioanario = findByPk(id)
+//     if(!funcioanario){
+//         return false
+//     }
+//     const index = dbFuncionarios.indexOf(funcioanario)
+//     dbFuncionarios[index] = funcionarioUpdate
+//     return true
+// }
+
+// export const dbFuncionarios = [
+//     new Funcionario(1, "Pedro", "pedro@example.com", "123456789", "1990-01-01", "1234567890", "adminpedro", "password123", "1234567890"),
+//     new Funcionario(2, "Vitor", "vitor@example.com", "987654321", "1992-05-10", "9876543210", "adminvitor", "securepass", "0987654321"),
+//     new Funcionario(3, "Sabrina", "sabrina@example.com", "456789123", "1988-12-15", "4567891230", "adminsabrina", "sabrina123", "4567891230"),
+//     new Funcionario(4, "Amanda", "amanda@example.com", "789123456", "1995-07-20", "7891234560", "adminamanda", "password123", "7891234560"),
+// ]
--- api/routes/FuncionarioRoutes.js
+++ api/routes/FuncionarioRoutes.js
@@ -4,5 +4,9 @@ import FuncionarioController from '../controllers/FuncionarioController.js'
 const FuncionarioRouter = express.Router()
 
 FuncionarioRouter.get('/', FuncionarioController.list)
+FuncionarioRouter.post('/', FuncionarioController.createFuncionario)
+FuncionarioRouter.get('/:id',FuncionarioController.getFuncionarioById)
+FuncionarioRouter.delete('/:id', FuncionarioController.destroyfuncionario)
+FuncionarioRouter.put('/:id', FuncionarioController.updateFuncionario)
 
 export default FuncionarioRouter
